4a2ab1a50c237bb68d52e92f62d50818098d40f1
ModerateUser <132522958+ModerateUser@users.noreply.github.com>
2025-11-21 17:11:08 -0500

    DEPRECATE: Rename audio_service_enhanced.py to mark for deletion

---
+++ services/DEPRECATED_audio_service_enhanced.py
@@ -0,0 +1,18 @@
+# DEPRECATED FILE
+# This file has been consolidated into audio_service.py
+# All functionality from this file has been merged into the main audio_service.py
+# This file is kept temporarily for reference and will be deleted in the next cleanup
+
+# DO NOT USE THIS FILE
+# Import from services.audio_service instead
+
+"""
+This file is deprecated and should not be used.
+All features have been merged into services/audio_service.py
+"""
+
+raise ImportError(
+    "audio_service_enhanced.py is deprecated. "
+    "All functionality has been consolidated into services/audio_service.py. "
+    "Please update your imports to use 'from services.audio_service import AudioService'"
+)
