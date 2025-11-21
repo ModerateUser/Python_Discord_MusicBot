a02e4714bb6118be8777761e447515acb7c4daee
ModerateUser <132522958+ModerateUser@users.noreply.github.com>
2025-11-21 07:23:58 -0500

    Add missing __init__.py for utils package

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,24 @@
-# utils/__init__.py
-"""Utility functions and helpers"""
+"""
+Utils package for Discord Music Bot
+Contains utility functions and helpers
+"""
+
+from .logger import setup_logger
+from .embeds import (
+    create_search_embed,
+    create_queue_embed,
+    create_nowplaying_embed,
+    create_playlist_list_embed,
+    create_playlist_show_embed,
+    create_help_embed
+)
+
+__all__ = [
+    'setup_logger',
+    'create_search_embed',
+    'create_queue_embed',
+    'create_nowplaying_embed',
+    'create_playlist_list_embed',
+    'create_playlist_show_embed',
+    'create_help_embed'
+]
