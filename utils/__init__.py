2c29f79707a2344d6919f8164fa9a7c6ff719b9d
ModerateUser <132522958+ModerateUser@users.noreply.github.com>
2025-11-21 05:09:29 -0500

    Add missing utils/__init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,2 @@
-"""
-Utility modules for the Discord Music Bot
-"""
-from .embeds import create_music_embed, create_error_embed, create_success_embed
-
-__all__ = ['create_music_embed', 'create_error_embed', 'create_success_embed']
+# utils/__init__.py
+"""Utility functions and helpers"""
