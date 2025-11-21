0a34fb422af513876020c4a845d156d7b29c1e34
ModerateUser <132522958+ModerateUser@users.noreply.github.com>
2025-11-21 07:23:04 -0500

    Add missing __init__.py for models package

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,8 @@
-# models/__init__.py
-"""Data models for songs and queues"""
+"""
+Models package for Discord Music Bot
+Contains data models and structures
+"""
+
+from .song import Song, MusicQueue
+
+__all__ = ['Song', 'MusicQueue']
