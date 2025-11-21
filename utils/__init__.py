14787d9dfc83f464e0d9327840d5002031475de1
ModerateUser <132522958+ModerateUser@users.noreply.github.com>
2025-11-21 04:40:55 -0500

    📦 ADD: utils/__init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,6 @@
+"""
+Utility modules for the Discord Music Bot
+"""
+from .embeds import create_music_embed, create_error_embed, create_success_embed
+
+__all__ = ['create_music_embed', 'create_error_embed', 'create_success_embed']
